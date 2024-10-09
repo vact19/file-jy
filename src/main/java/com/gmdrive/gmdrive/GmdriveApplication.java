c82860ae4cb46bb0471eec9d34cb467769cf4b0c
jo.sw <chj6703@gmail.com>
2024-10-09 20:43:20 +0900

    init

---
+++ src/main/java/com/gmdrive/gmdrive/GmdriveApplication.java
@@ -0,0 +1,13 @@
+package com.gmdrive.gmdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmdriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmdrive/gmdrive/GmdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmdrive.gmdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmdriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
