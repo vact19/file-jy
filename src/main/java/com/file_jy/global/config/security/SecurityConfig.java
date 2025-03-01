0573b617b5f248a95741cc950511ef2458399a33
jo.sw <chj6703@gmail.com>
2025-03-01 14:07:40 +0900

    웹서버 Origin CORS 허용

--- src/main/java/com/file_jy/global/config/security/SecurityConfig.java
+++ src/main/java/com/file_jy/global/config/security/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -30,27 +35,35 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.httpBasic().disable()
                 .csrf().disable()
-//                .cors().and()
+                .cors().configurationSource(corsConfigurationSource()).and()
                 .formLogin().disable() // 폼 로그인 비활성화. JWT 필터로 대체
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeHttpRequests()
-                /*
-                permitAll()은 필터링 예외가 아니라, 필터링을 모두 거치고 나서 Auth 객체가 없어도 접근을 허용하는 것이다.
-                그래서 GET 요청에서도 SecurityContext의 Authentication 객체를 활용 가능하다. 물론 해당 객체는 null일 수도 있다.
-                */
-                .requestMatchers(HttpMethod.OPTIONS).permitAll() // OPTIONS 요청은 토큰 검증 예외
-                .requestMatchers(HttpMethod.GET, "/error").permitAll() // OPTIONS 요청은 토큰 검증 예외
-                .requestMatchers(HttpMethod.POST, "/users").permitAll() // 회원가입 요청은 토큰 검증 예외
-                .requestMatchers(HttpMethod.POST, "/users/sign-in").permitAll() // 로그인 요청은 토큰 검증 예외
-                .anyRequest().authenticated()
-                .and()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // OPTIONS 메서드 모든 경로 허용
+                        .requestMatchers(HttpMethod.GET, "/error").permitAll() // GET /error 허용. 전역 예외처리를 타지 못해서 리다이렉트될 경우.
+                        .requestMatchers(HttpMethod.POST, "/users", "/users/sign-in").permitAll()
+                        .anyRequest().authenticated()
+                )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling()
                 .authenticationEntryPoint(custom401AuthEntryPoint)
                 .accessDeniedHandler(customAccessDeniedHandler)
         ; // 인증 예외
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
